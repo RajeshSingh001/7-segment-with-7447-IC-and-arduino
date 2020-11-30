fe8a4c72b8c309ac42c6ac380af251424f389405
Dr. Rajesh Singh <75157838+RajeshSingh001@users.noreply.github.com>
2020-11-30 09:55:59 +0530

    arduino ide code and proteus model

---
+++ _7_SEG_7447/_7_SEG_7447.ino
@@ -0,0 +1,30 @@
+
+
+int a = 13;
+int b = 12;
+int c = 11;
+int d =10;
+void setup()
+{                
+  // initialize the digital pin as an output.
+  pinMode(a, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(d, OUTPUT);
+   
+}
+
+void loop()
+{
+  digitalWrite(a, LOW);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(b, LOW);
+  digitalWrite(c, LOW);
+ digitalWrite(d, LOW);
+ 
+  delay(1000);               // wait for a second
+   digitalWrite(a, LOW);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(b, LOW);
+  digitalWrite(c, LOW);
+ digitalWrite(d, HIGH);
+    delay(1000);           
+}
